16a41654c0e16590b41e7a066fe8d1d36108edd1
jjhp516 <jjhp516@gmail.com>
2018-09-30 03:55:04 -0600

    page update

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('test', function() {
-    console.log('testing...')
-})
-
-
-gulp.task('style', function() {
+gulp.task('styles', function() {
     gulp.src('./sass/app.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./css'))
-})
+        .pipe(gulp.dest('./css/'))
+});
 
-gulp.task('watch', function(){
-    gulp.watch('./sass/**/*.scss', ['style'])
-})
+
+gulp.task('watch',function() {
+    gulp.watch('./sass/**/*.scss',['styles']);
+});
 
