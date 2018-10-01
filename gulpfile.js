989602ccb34cbbefb944c6dd6295e2c34359ab33
Jason Park <803456@ACDM.DS.SAIT.CA>
2018-10-01 09:36:45 -0600

    added images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+// var r = require('tiny-lr')
+// var refresh = require('gulp-livereload')
+// var nodemon = require('gulp-nodemon')
+// var wait = require('../gulp-wait')
+// var server = lr();
 
 gulp.task('styles', function() {
     gulp.src('./sass/app.scss')
@@ -11,4 +16,25 @@ gulp.task('styles', function() {
 gulp.task('watch',function() {
     gulp.watch('./sass/**/*.scss',['styles']);
 });
+
+
+
+// gulp.task('dev', function () {
+ 
+//     gulp.src('./index.js')
+//       .pipe(nodemon());
+   
+//     server.listen(35729, function (err) {
+   
+//         if (err) return console.log(err);
+   
+//         gulp.watch('./app/views/**/*.html', function (e) {
+//         gulp.src(e.path)
+//           .pipe(wait(1500))
+//           .pipe(refresh(server));
+//       });
+   
+//     });
+   
+//   })
 
